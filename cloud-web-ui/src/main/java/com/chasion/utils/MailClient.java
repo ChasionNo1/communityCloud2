c9530813afe9c5cc0a9228299b66f194ff7a6f54
ChasionNo1 <185897780@qq.com>
2025-03-29 18:40:50 +0800

    修改了一些内容，为部署做准备

--- cloud-web-ui/src/main/java/com/chasion/utils/MailClient.java
+++ cloud-web-ui/src/main/java/com/chasion/utils/MailClient.java
@@ -1,5 +1,6 @@
 package com.chasion.utils;
 
+import jakarta.annotation.Resource;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.slf4j.Logger;
@@ -16,7 +17,7 @@ public class MailClient {
 
     private static final Logger logger = LoggerFactory.getLogger(MailClient.class);
 
-    @Autowired
+    @Resource
     private JavaMailSender mailSender;
 
     @Value("${spring.mail.username}")
